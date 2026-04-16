9a984d255b0f6af0f72708c8b1f3e19db8d0b8f0
shreyamittal10 <158428971+shreyamittal10@users.noreply.github.com>
2026-04-16 20:55:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 app.get("/test-db", async (req, res) => {
   try {
